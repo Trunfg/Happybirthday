6a05560454822206c8e93002a9cc39e2d79d9e33
buitruongar13 <2051063856@e.tlu.edu.vn>
2024-07-26 22:52:03 +0700

    t

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const nameGirl = 'Chau Anh 18+🤍🎂🐰';
+const giftUrl = '';
 const eventName = 'Chúc mừng sinh nhật';
 const titleCard = 'Happy birthday';
 const contentCard = 'Chúc mừng sinh nhật nữ hoàng của anh. Chúc những dự định tương lai của em ấp ủ sẽ thành sự thật. Chúc em luôn vui vẻ và trưởng thành trên những chặng đường mà em bước. Cảm ơn em vì đã luôn đồng hành cùng anh. Anh mong dù chúng ta mệt cùng mệt, khổ cùng khổ, ấm êm cùng ấm êm. Anh không mong cầu xa hoa chỉ mong rằng giữa bao nhiêu thăng trầm của cuộc sống vẫn có em làm động lực cho anh cố gắng. Chúc em tuổi 18 tràn đầy năng lượng. Gửi em những lời chúc thân thương nhất nhân ngày sinh nhật của em. Happy birthday Chau Anh 18+!!! Anh yêu nữ hoàng Chau Anhh🤍🐰';
